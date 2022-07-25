2fddae60f9715b0e319c03cb6ac322e4b4878283
chirstiankaikai <ckaikai19@gmail.com>
2022-07-24 22:34:47 -0400

    added deleted

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -52,7 +52,12 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(data => res.json(data))
 });
 
 module.exports = router;
