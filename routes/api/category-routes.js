8a9e33c48a07dc3e97b6c05d9f3dffc39ad57f89
chirstiankaikai <ckaikai19@gmail.com>
2022-07-24 22:19:35 -0400

    added get route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,10 +14,6 @@ router.get('/', (req, res) => {
         ]
   })
     .then(dbCategoryData => res.json(dbCategoryData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
 });
 
 router.get('/:id', (req, res) => {
