0a08ce2d5fa11c4b5c39824f3366df2d89699aef
chirstiankaikai <ckaikai19@gmail.com>
2022-07-24 22:17:25 -0400

    fixed bugs

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
-require('dotenv').config({ path: '../.env' });
+// require('dotenv').config({ path: '../.env' });
 
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME, 
-  process.env.DB_USER, 
-  process.env.DB_PW, 
+  'ecommerce_db', 
+  'root', 
+  'password', 
   {
       host: 'localhost',
       dialect: 'mysql',
